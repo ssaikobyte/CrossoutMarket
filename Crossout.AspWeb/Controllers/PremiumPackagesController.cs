d9d0038b9b38c32b14b73cbb15cd22bb6112547b
SilentNyte <silentnyte@msn.com>
2021-09-21 11:58:40 -0400

    Initial commit for PreimumPack migration to sql

--- Crossout.AspWeb/Controllers/PremiumPackagesController.cs
+++ Crossout.AspWeb/Controllers/PremiumPackagesController.cs
@@ -24,7 +24,28 @@ namespace Crossout.AspWeb.Controllers
         }
 
         [Route("packs")]
-        public IActionResult Packages()
+        public IActionResult Packages(int id)
+        {
+            Language lang = this.ReadLanguageCookie(sql);
+            this.RegisterHit("Packs");
+
+            sql.Open(WebSettings.Settings.CreateDescription());
+
+            DataService db = new DataService(sql);
+
+            try
+            {
+                var packagesCollection = db.SelectAllPremiumPackages();
+
+                return View("packages", packagesCollection);
+            }
+            catch (Exception ex)
+            {
+                return Redirect("/");
+            }
+        }
+
+        public IActionResult PackagesOld()
         {
             Language lang = this.ReadLanguageCookie(sql);
             this.RegisterHit("Packs");
---
+++ Crossout.AspWeb/Pocos/PremiumPackagePoco.cs
@@ -0,0 +1,29 @@
+﻿using NPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crossout.AspWeb.Pocos
+{
+    [TableName("premiumpackage")]
+    [PrimaryKey("id")]
+    public class PremiumPackagePoco
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("key")]
+        public string Key { get; set; }
+
+        [Column("appid")]
+        public int SteamAppID { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("coins")]
+        public int RawCoins { get; set; }
+    }
+
+}
