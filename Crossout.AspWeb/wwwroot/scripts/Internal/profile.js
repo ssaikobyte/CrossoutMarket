c6460ac58aaa16e8501ea754fb32c300beb9b50d
moguevara <morg.hall@gmail.com>
2021-10-24 19:29:42 -0400

    got drill down working

--- Crossout.AspWeb/wwwroot/scripts/Internal/profile.js
+++ Crossout.AspWeb/wwwroot/scripts/Internal/profile.js
@@ -13,12 +13,9 @@ success: function (json) {
 function build_drilldown(id, title, drilldown_data) {
 
     var series_data = populate_series_data(drilldown_data);
-    var drilldown_data = populate_drilldown_data(drilldown_data);
+    var drilldown_series_data = populate_drilldown_data(drilldown_data);
     var favorite = series_data.reduce((a, b) => a.y > b.y ? a : b);
-
-    console.log(series_data);
-    console.log(drilldown_data);
-
+   
     Highcharts.chart(id, {
         chart: {
             type: 'pie'
@@ -30,11 +27,11 @@ function build_drilldown(id, title, drilldown_data) {
             enabled: false
         },
         subtitle: {
-            text: 'Preference ' + favorite.name + '(' + Math.round(favorite.y) + '%)'
+            text: 'Preference ' + favorite.name + ' (' + Math.round(favorite.y, 2) + '%)'
         },
         accessibility: {
             announceNewData: {
-                enabled: false
+                enabled: true
             },
             point: {
                 valueSuffix: '%'
@@ -50,9 +47,8 @@ function build_drilldown(id, title, drilldown_data) {
         },
         tooltip: {
             headerFormat: '<span style="font-size:11px">{point.name}</span><br>',
-            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.1f}%</b> of total<br/>'
         },
-
         series: [
             {
                 name: title,
@@ -61,47 +57,49 @@ function build_drilldown(id, title, drilldown_data) {
             }
         ],
         drilldown: {
-            series: drilldown_data
+            series: drilldown_series_data
         }
     });
 }
 
 function populate_series_data(drilldown_data) {
-    var total = 0;
     var data = [];
+    var total = 0;
 
     for (let i = 0; i < drilldown_data.length; i++) {
         total += drilldown_data[i]["count"];
+    }
 
+    for (let i = 0; i < drilldown_data.length; i++) {
         if (data.filter(function (e) { return e.name === drilldown_data[i]["type"]; }).length > 0) {
-            data[data.findIndex(x => x.name === drilldown_data[i]["type"])].y += drilldown_data[i]["count"];
+            data[data.findIndex(x => x.name === drilldown_data[i]["type"])].y += (drilldown_data[i]["count"] / total) * 100;
         }
         else {
             data.push({
                 name: drilldown_data[i]["type"],
-                y: drilldown_data[i]["count"],
+                y: (drilldown_data[i]["count"] / total) * 100,
                 drilldown: drilldown_data[i]["type"]
             });
         }
     }
-
-    for (let i = 0; i < data.length; i++) {
-        data[i].y = (data[i].y / total) * 100;
-    }
-
     return data;
 }
 
 function populate_drilldown_data(drilldown_data) {
     var data = [];
+    var total = 0;
+
+    for (let i = 0; i < drilldown_data.length; i++) {
+        total += drilldown_data[i]["count"];
+    }
 
     for (let i = 0; i < drilldown_data.length; i++) {
         var found_series = false;
 
         for (let j = 0; j < data.length; j++) {
-            if (data[j].series.name == drilldown_data[i]["type"]) {
-                data[j].series.data.push(
-                    [drilldown_data[i]["name"], drilldown_data[i]["count"]]
+            if (data[j].name == drilldown_data[i]["type"]) {
+                data[j].data.push(
+                    [drilldown_data[i]["name"], (drilldown_data[i]["count"] / total) * 100]
                 );
                 found_series = true;
                 break;
@@ -109,15 +107,11 @@ function populate_drilldown_data(drilldown_data) {
         }
 
         if (!found_series) {
-            var obj = {
-                'series': {
-                    'name': drilldown_data[i]["type"],
-                    'id': drilldown_data[i]["type"],
-                    'data': [[drilldown_data[i]["name"], drilldown_data[i]["count"]]]
-                }
-            }
-
-            data.push(obj);
+            data.push({
+                'name': drilldown_data[i]["type"],
+                'id': drilldown_data[i]["type"],
+                'data': [[drilldown_data[i]["name"], (drilldown_data[i]["count"] / total) * 100]]
+            });
         }
     }
 
