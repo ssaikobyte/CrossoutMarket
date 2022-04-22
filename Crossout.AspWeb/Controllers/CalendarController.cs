83cdeb6e526299e5734197a137547b17c7dcda1c
Stiffi <git@mxsd.de>
2022-04-22 10:36:49 +0200

    Add calendar

---
+++ Crossout.AspWeb/Controllers/CalendarController.cs
@@ -0,0 +1,35 @@
+﻿using Crossout.AspWeb.Helper;
+using Crossout.AspWeb.Models.General;
+using Crossout.AspWeb.Models.Language;
+using Crossout.AspWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZicoreConnector.Zicore.Connector.Base;
+
+namespace Crossout.AspWeb.Controllers
+{
+    public class CalendarController : Controller
+    {
+        SqlConnector sql = new SqlConnector(ConnectionType.MySql);
+
+        [Route("calendar")]
+        public IActionResult Calendar()
+        {
+            sql.Open(WebSettings.Settings.CreateDescription());
+
+            DataService db = new DataService(sql);
+
+            Language lang = this.ReadLanguageCookie(sql);
+
+            CalendarModel model = new CalendarModel();
+            model.Localizations = db.SelectFrontendLocalizations(lang.Id, "calendar");
+
+            this.RegisterHit("Calendar");
+
+            return View("calendar", model);
+        }
+    }
+}
---
+++ Crossout.AspWeb/Models/General/CalendarModel.cs
@@ -0,0 +1,13 @@
+﻿using Crossout.AspWeb.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crossout.AspWeb.Models.General
+{
+    public class CalendarModel : BaseViewModel, IViewTitle
+    {
+        public string Title => "Calendar";
+    }
+}
