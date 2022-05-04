fbbb5b39f1fcd9446a568c79220f23e2eb15c952
Stiffi <git@mxsd.de>
2022-05-04 11:23:37 +0200

    Move calendar js into new file

---
+++ Crossout.AspWeb/wwwroot/scripts/Internal/calendar.js
@@ -0,0 +1,26 @@
+﻿var lang = Cookies.get("language");
+if (lang === undefined || !lang.match("en|ru"))
+    lang = "en";
+
+var calendarEl = document.getElementById("calendar");
+var calendar = new FullCalendar.Calendar(calendarEl, {
+    locale: lang,
+    height: "auto",
+    initialView: "timeGridDay",
+    headerToolbar: {
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay",
+    },
+    eventSources: [{
+        url: "/schedules/brawls.ics",
+        format: "ics"
+    },
+    {
+        url: "/schedules/clanwars.ics",
+        format: "ics",
+        color: "#D46722"
+    }]
+});
+
+calendar.render();
